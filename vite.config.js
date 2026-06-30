4dc323410fe3ae8f3f966e23862608e2ec55670d
Ojas Shrivastava <ojasshrivastava1008@gmail.com>
2026-06-30 20:49:03 +0530

    fix: update base path casing in vite config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/gericht-restaurant/",
+  base: "/Gericht-Restaurant/",
   plugins: [
     react(),
     tailwindcss(),
